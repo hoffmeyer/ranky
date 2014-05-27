e14401c58f1f98c6429fac668986086299a78890
Flemming Hoffmeyer <flemming@schantz.com>
2014-05-27 21:22:03 +0200

    now creates, stores events, and loads events back up when the server starts

---
+++ events/events.js
@@ -0,0 +1,35 @@
+module.exports = (function(){
+  'use strict';
+
+  var nextId = 1;
+  // TODO: make genereal function that creates a basic event with id and datetime
+
+  return {
+    setNextId: function(newId){
+      if(newId > nextId) {
+        nextId = newId;
+      } else {
+        console.log('new nextId for events must be greater than old id');
+      }
+    },
+    createPlayer: function(name) {
+      return {
+        id: nextId++,
+        type: 'createPlayerEvent',
+        playerName: name
+      }
+    },
+    registerMatch: function(player1Id, score1, player2Id, score2){
+      console.log(player1Id + ' : ' + player2Id);
+      return {
+        id: nextId++,
+        type: 'registerMatchEvent',
+        player1Id:  player1Id,
+        score1:     score1,
+        player2Id:  player2Id,
+        score2:     score2
+      };
+    }
+  };
+
+})();
