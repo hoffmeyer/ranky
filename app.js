a6459a182e1809c85f95576df9b8568e5d5dcd23
Flemming Hoffmeyer <flemming.b.h@gmail.com>
2014-06-25 22:46:09 +0200

    cleanup of test data generation and loading from database

--- app.js
+++ app.js
@@ -2,10 +2,74 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     routes = require('./routes/routes.js'),
-    app = express();
+    app = express(),
+    db = require('monk')('localhost/ranky'),
+    dbEvent = db.get('events'),
+    ranky = require('./logic/ranky.js'),
+    events = require('./events/events.js'),
+    _ = require('underscore')._;
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use('/', routes);
+
+// generate test dummy data
+var generateTestData = function() {
+  'use strict';
+  var names = [
+    'Knuspar',
+    'Olfie',
+    'Muhammed',
+    'Gunter',
+    'Ingeborg',
+    'Egon',
+    'Sweinhund',
+    'Johanes',
+    'Ib',
+    'Lulu',
+    'Rose',
+    'Bolcheface',
+    'Bomberman',
+    'NicerDicer',
+    'Numseman'
+  ];
+
+  names.map(function(name) {
+    console.log('Generating test player ' + name);
+    ranky.handleEvent(events.createPlayer(name));
+  });
+
+  var numMatches = 100,
+      remainingMatches = 100;
+
+  var genId = function() {
+    return Math.floor((Math.random() * names.length)+1);
+  };
+  var genScore = function() {
+    return Math.floor((Math.random() * 11) );
+  };
+  do {
+    console.log('Generating test match ' + (numMatches-remainingMatches+1) + '/' + numMatches);
+    ranky.handleEvent(events.registerMatch([genId(), genId()], genScore(), [genId(),genId()], genScore()));
+    remainingMatches--;
+  } while(remainingMatches> 0);
+};
+
+// load data from database
+var loadEventsFromDB = function() {
+  'use strict';
+  dbEvent.find({},{sort: {id: 1}}, function(err, docs){
+    _.map(docs, function(event){
+      ranky.handleEvent(event);
+      events.setNextId(event.id+1);
+    });
+
+    if(!docs.length){
+      generateTestData();
+    }
+  });
+};
+
+loadEventsFromDB();
 
 // start server
 var server = app.listen(3000, function() {
--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,7 @@
 var express = require('express'),
     router = express.Router(),
     ranky = require('../logic/ranky.js'),
-    db = require('monk')('localhost/ranky'),
-    dbEvent = db.get('events'),
-    events = require('../events/events.js'),
-    _ = require('underscore')._;
-
-
-// TODO: nasty shit, this function should not just be dumped here to import events from database
-(function() {
-  dbEvent.find({},{sort: {id: 1}}, function(err, docs){
-    _.map(docs, function(event){
-      ranky.handleEvent(event);
-      events.setNextId(event.id+1);
-    });
-  });
-})();
+    events = require('../events/events.js');
 
 router.get('/', function(req, res) {
   'use strict';
@@ -56,43 +42,3 @@ router.post('/match', function(req, res) {
 });
 
 module.exports = router;
-
-// for testing only.. remove when datbase has been added
-/*
-var names = [
-  'Knuspar',
-  'Olfie',
-  'Muhammed',
-  'Gunter',
-  'Ingeborg',
-  'Egon',
-  'Sweinhund',
-  'Johanes',
-  'Ib',
-  'Lulu',
-  'Rose',
-  'Bolcheface',
-  'Bomberman',
-  'NicerDicer',
-  'Numseman'
-];
-
-names.map(function(name) {
-  'use strict';
-  ranky.handleEvent(events.createPlayer(name));
-});
-
-var numMatches = 100;
-var genId = function() {
-  'use strict';
-  return Math.floor((Math.random() * names.length)+1);
-};
-var genScore = function() {
-  'use strict';
-  return Math.floor((Math.random() * 11) );
-};
-do {
-  ranky.handleEvent(events.registerMatch([genId(), genId()], genScore(), [genId(),genId()], genScore()));
-  numMatches--;
-} while(numMatches > 0);
-*/
