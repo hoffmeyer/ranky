53ad093fbbc94d9a04c1f6a44b7167eed125b48f
Flemming Hoffmeyer <flemming@schantz.com>
2014-09-04 10:13:11 +0200

    added a few tests to the scoring engine

--- test/modules/test_scoringEngine.js
+++ test/modules/test_scoringEngine.js
@@ -3,20 +3,92 @@ var should = require('should'),
     bus = require('../../logic/eventBus.js'),
     scoringEngine = require('../../modules/ScoringEngine.js');
 
-bus.register(scoringEngine);
+describe('scoringEngine', function() {
 
-bus.post({
-    type: 'scoreMatchEvent',
-    team1: {
-        players: [{id: 1}],
-        score: 10
-    },
-    team2: {
-        players: [{id: 2}],
-        score: 5
-    },
-    callback: function(scores) {
-        scores.should.eql({1: 25, 2: -25});
-    }
+    bus.register(scoringEngine);
+
+    describe('scoreMatchEvent', function(){
+
+        describe('team1 wins', function() {
+            var scores;
+            bus.post({
+                type: 'scoreMatchEvent',
+                team1: {
+                    players: [{id: 1}],
+                    score: 10
+                },
+                team2: {
+                    players: [{id: 2}],
+                    score: 5
+                },
+                callback: function(s) {
+                    scores = s;
+                }
+            });
+
+            it('should give player1 25 points and player2 -25 points', function(){
+                scores.should.eql({1: 25, 2: -25});
+            });
+        });
+
+        describe('team2 wins', function() {
+            var scores;
+            bus.post({
+                type: 'scoreMatchEvent',
+                team1: {
+                    players: [{id: 1}],
+                    score: 5 
+                },
+                team2: {
+                    players: [{id: 2}],
+                    score: 10
+                },
+                callback: function(s) {
+                    scores = s;
+                }
+            });
+
+            it('should give player1 -25 points and player2 25 points', function(){
+                scores.should.eql({1: -25, 2: 25});
+            });
+        });
+
+        describe('tied match', function(){
+            var scores;
+            bus.post({
+                type: 'scoreMatchEvent',
+                team1: {
+                    players: [{id: 1}],
+                    score: 10
+                },
+                team2: {
+                    players: [{id: 2}],
+                    score: 10
+                },
+                callback: function(s) {
+                    scores = s;
+                }
+            });
+
+            it('should give all players zero points if the match is tied', function(){
+                scores.should.eql({1: 0, 2: 0});
+            });
+        });
+    });
+
+
+    describe('unknownEvent', function() {
+        var test;
+        bus.post({
+            type: 'unknownEvent',
+            callback: function(){
+                test = 'test';
+            }
+        });
+
+        it('should ignore an unknown event', function() {
+            (test === undefined).should.be.true;
+        });
+    })
 });
 
