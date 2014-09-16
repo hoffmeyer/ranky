5b2724ec6004b91b1c7f9440a44520183547d767
Flemming Hoffmeyer <flemming.b.h@gmail.com>
2014-09-16 22:24:23 +0200

    db uri in variable

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // initialize app
-var express = require('express'),
+var dbUri = process.env.MONGOLAB_URI || 'localhost/ranky';
+    express = require('express'),
     app = express(),
     http = require('http').Server(app),
     bodyParser = require('body-parser'),
     routes = require('./routes/routes.js'),
-    db = require('monk')(process.env.MONGOLAB_URI || 'localhost/ranky'), // process.env.MONGOHQ_URL supplied by Heroku
+    db = require('monk')(dbUri), // process.env.MONGOHQ_URL supplied by Heroku
     io = require('socket.io')(http),
     _ = require('underscore')._,
     dbEvent = db.get('events'),
     ranky = require('./logic/ranky.js')(io),
     events = require('./events/events.js');
 
-console.log('process.env.MONGOLAB_URI:');
-console.log(process.env.MONGOLAB_URI);
+console.log('dbUri:');
+console.log(dbUri);
 console.log('db:');
 console.log(db);
 
