faa77f834104afb3b051423611db0354e31c74e9
Flemming Hoffmeyer <flemmingbh@gmail.com>
2014-12-14 20:10:19 +0100

    added browserify

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    hbsfy = require('browserify-handlebars');
+
+var watcher = gulp.watch('websrc/**/*.js', ['browserify']);
+watcher.on('change', function(event){
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks');
+});
+
+gulp.task('default', function(){
+
+});
+
+gulp.task('browserify', function(){
+    gulp.src('websrc/rankyui.js')
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: true,
+            transform: [hbsfy]
+        }))
+        .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('less', function(){
+
+});
