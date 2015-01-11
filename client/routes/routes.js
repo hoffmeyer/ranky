57f8d557fef004e995f1e11f98fe7426cbd99472
Flemming Hoffmeyer <flemming@festina-lente.dk>
2015-01-11 19:08:23 +0100

    two functioning pages with navigation

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -2,21 +2,25 @@ var playerList = require('../playerList.js'),
     newMatch = require('../newMatch.js');
 
 module.exports = function(element){
-    playerList(element);
-    window.onpopstate = function(){
+
+    var updateContent = function() {
         var url = window.location.hash.substr(1),
             view;
 
         switch(url){
             case 'newMatch':
                 view = newMatch;
-                console.log('newMatch');
                 break;
             default:
                 view = playerList;
-                console.log('playerList');
                 break;
         }
         view(element);
     };
+
+    window.onpopstate = function(){
+        updateContent();
+    };
+
+    updateContent();
 };
