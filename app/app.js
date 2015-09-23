f99e9307aecb405dc8fee525e524ef4f1770f218
Flemming Hoffmeyer <flemmingbh@gmail.com>
2015-09-23 22:09:33 +0200

    cleanup in app.js

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ var express = require('express'),
     io = require('socket.io')(http),
     pg = require('pg'),
     connectionString = process.env.DATABASE_URL || 'postgres://ranky:12345q@db/ranky',
+    eventLoader = require('./logic/eventLoader')(connectionString),
     ranky = require('./logic/ranky.js')(io, pg, connectionString),
     events = require('./events/events.js');
 
@@ -36,11 +37,11 @@ app.use(function (req, res, next) {
 // set router
 app.use('/', routes);
 
-// serve resources from public folder eg css an client side js
+// serve resources from public folder eg css and client side js
 app.use(express.static(__dirname + '/public'));
 
 var startHttpServer = function(){
-    var server = http.listen(process.env.PORT || 3000, function(err) { // process.env.PORT supplied by Heroku
+    var server = http.listen(process.env.PORT || 3000, function(err) {
         if(err){
             console.log('Could not start server on %d', server.address().port);
             console.trace(err);
@@ -50,47 +51,9 @@ var startHttpServer = function(){
     });
 };
 
-// load data from database
-var loadEventsFromDB = function() {
-    console.log( 'Fetching stored evetns from db...');
-    pg.connect(connectionString, function(err, client, done) {
-        if(err){
-            console.error('Error fetching client from pool using connString ' + connectionString, err);
-        } else {
-            client.query('CREATE TABLE IF NOT EXISTS EVENTS( ID    INT NOT NULL PRIMARY KEY, DATA    JSON )', function(err, result){ if(err) {
-                    console.error('Failed to create table events');
-                }
-                console.log('Created table events if not existing');
-            });
-            client.query('SELECT * FROM events', function(err, result){
-                done(); // release client back to conn pool
-                if(err){
-                    console.error('Error running query', err);
-                }
-                console.log('Queried ' + result.rows.length + ' events from database.');
-                console.log( 'Applying events...');
-                var i = 0;
-                // function for chaining the events to roll them on synchronously
-                var loadEvent = function(event){
-                    events.setNextId(event.id+1);
-                    i++;
-                    ranky.handleEvent(event).then(function(){
-                        if(i < result.rows.length){
-                            loadEvent(result.rows[i].data);
-                        } else {
-                            console.log('Loaded ' + i + ' events from db');
-                            startHttpServer();
-                        }
-                    });
-                };
-                if(result.rows && result.rows.length > 0){
-                    loadEvent(result.rows[0].data);
-                } else {
-                    startHttpServer();
-                }
-            });
-        }
-    });
-};
-
-loadEventsFromDB();
+eventLoader.initialize()
+.then(eventLoader.loadEvents)
+.then(startHttpServer)
+.catch(function(error){
+  console.error(error);
+});
